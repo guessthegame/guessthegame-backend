bc2bb28a89e741d76267553a5817f4e1773cd58b
Paulin Trognon <paulin.trognon@enexten.com>
2021-10-21 17:11:11 +0200

    display app version in hello world

--- src/modules/app.controller.ts
+++ src/modules/app.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get } from '@nestjs/common'
 
+import { version } from '../../package.json'
+
 class AppControllerResponse {
   app: string
+  version: string
 }
 
 @Controller('/')
 export class AppController {
   @Get('/')
   helloWold(): AppControllerResponse {
-    return { app: 'guessthegame-backend' }
+    return { app: 'guessthegame-backend', version }
   }
 }
